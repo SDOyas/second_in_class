8681ebb41564f3f06b41cec0fa77ea3790804224
Simon Oyas <sdoyas@cpp.edu>
2022-03-09 17:56:00 -0800

    Misspelled Ptr LOL

--- employeeOne.c
+++ employeeOne.c
@@ -1,24 +1,24 @@
 #include <string.h>
 #include "employee.h"
 
-PrtToEmployee searchEmployeeByNumber(PrtToConstEmployee ptr, int tableSize, long targetNumber)
+PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int tableSize, long targetNumber)
 {
     const PtrToConstEmployee endPtr = ptr + tableSize;
     for(; ptr < endPtr; ptr++)
     {
         if(ptr->number == targetNumber)
-            return (PrtToEmployee) ptr;
+            return (PtrToEmployee) ptr;
     }
     return NULL;
 }
 
-PrtToEmployee searchEmployeeByName(PrtToConstEmployee ptr, int tableSize, char * targetName)
+PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int tableSize, char * targetName)
 {
     const PtrToConstEmployee endPtr = ptr + tableSize;
     for(; ptr < endPtr; ptr++)
     {
         if(strcmp(ptr->name, targetName) == 0)
-            return (PrtToEmployee) ptr;
+            return (PtrToEmployee) ptr;
     }
     return NULL;
 }
